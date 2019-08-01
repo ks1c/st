48c76a41c6c4b4624297947b0a82add0df7ed239
ks1c <ksic3784@gmail.com>
2019-07-31 20:41:17 -0400

    change font and border size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:size=14:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Inconsolata:size=17:antialias=true:autohint=true";
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
