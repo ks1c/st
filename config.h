1acc08aa502771cfe9d9c0e91820babc32101c0d
ks1c <iamks1c@gmail.com>
2020-03-23 21:50:18 -0400

    change font to fira code

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:size=17:antialias=true:autohint=true";
+static char *font = "Fira Code:size=14:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
